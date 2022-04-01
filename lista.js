453bbb14fdda394b402d0146ce577da2363d7d5c
joaoalmeida010280 <joaoalmeida010280@gmail.com>
2022-04-01 08:50:48 -0300

    adicionei os arquivos HTML e JS que foram usados para criar uma lista de tarefas

---
+++ lista.js
@@ -0,0 +1,44 @@
+function Submit(){
+    const lista = document.querySelector('[data-task]')
+    const valor = document.querySelector('[data-form-input]')
+    const novo_item_lista = document.createElement("li")
+    
+    novo_item_lista.innerHTML = valor.value
+    lista.appendChild (novo_item_lista)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction(){
+//     console.log("teste inicial");
+// }
+
+// function addItem(){
+//     const inputItem = document.querySelector('[data-form-input]')
+//     const valorItem = inputItem.value
+
+//     const listaDeItems = document.querySelector('[data-task]')
+
+//     novaLabel = document.createElement('label')
+//     novaLabel.innerText = valorItem
+    
+//     novoItem = document.createElement('li')
+
+//     // novoItem.appendChild(criarBotaoConcluir())
+//     novoItem.appendChild(novaLabel)
+//     // novoItem.appendChild(criarBotaoDelete())
+
+//     listaDeItems.appendChild(novoItem)
+
+//     inputItem.value = ""
+//}

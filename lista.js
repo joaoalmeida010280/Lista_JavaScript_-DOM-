cb9a067f05bc6fa9e8ea0526fb71d06bd4fd569a
joaoalmeida010280 <joaoalmeida010280@gmail.com>
2022-04-22 14:25:34 -0300

    Arquivos da Lista com o botão editar

--- lista.js
+++ lista.js
@@ -1,10 +1,62 @@
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
+function EditItem(){
+    this.parentElement.remove()
+    let novo_edit = prompt("Altere a tarefa");
+
+    const lista = document.querySelector('[data-task]')
+    const novo_item_lista = document.createElement("li")
+    novo_item_lista.className = "list-group-item";
+
+    novo_item_lista.innerHTML = novo_edit
+ 
+    novo_item_lista.appendChild(CriarBotaoDelete())
+    novo_item_lista.appendChild(CriarBotaoEdit())
+    lista.appendChild (novo_item_lista)
+
+
+
+    document.getElementById("item").value = "" 
+
+}
+function CriarBotaoDelete(){
+    const botao_delete =  document.createElement('button')
+    botao_delete.classList.add("btn", "btn-danger")
+    botao_delete.innerHTML = "Delete"
+    botao_delete.type = "button"
+    botao_delete.addEventListener("click", DeleteItem);
+
+    return botao_delete
+}
+
+function CriarBotaoEdit(){
+    const botao_edit =  document.createElement('button')
+    botao_edit.classList.add("btn", "btn-danger")
+    botao_edit.innerHTML = "Edit"
+    botao_edit.type = "button"
+    botao_edit.addEventListener("click", EditItem);
+
+    return botao_edit
+}
+
 function Submit(){
     const lista = document.querySelector('[data-task]')
     const valor = document.querySelector('[data-form-input]')
+
     const novo_item_lista = document.createElement("li")
-    
+    novo_item_lista.className = "list-group-item";
+
     novo_item_lista.innerHTML = valor.value
+ 
+    novo_item_lista.appendChild(CriarBotaoDelete())
+    novo_item_lista.appendChild(CriarBotaoEdit())
     lista.appendChild (novo_item_lista)
+
+   
+
+    document.getElementById("item").value = ""
 }
 
 
